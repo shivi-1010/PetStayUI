1a5cd8b65434c5cc1447490571d89d012e773994
SHIVANI VARU <you@example.com>
2025-05-17 00:06:13 -0400

    Update Files

--- assets/js/landingscript.js
+++ assets/js/landingscript.js
@@ -5,8 +5,9 @@ document.getElementById('left-button').addEventListener('click', function() {
 
 // Redirect to Admin Dashboard page
 document.getElementById('right-button').addEventListener('click', function() {
-  window.location.href = '/admin_dashboard.html';
+  window.location.href = '/admin-frontend/admin_dashboard.html';
 });
+
 
 // Hover effects remain the same
 const content = document.querySelector(".content");
