b12a88596553b048dfd9e5d0f834e3a5533ca1fa
SHIVANI VARU <you@example.com>
2025-08-09 02:51:05 -0400

    adding ai chatbot

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,14 @@
 //index.js
 
 // Redirect to Customer Booking page
-// 🔗 Redirect to Customer Booking page
+//  Redirect to Customer Booking page
 document.getElementById('left-button')?.addEventListener('click', () => {
   window.location.href = '/customer/new-booking.html';
+});
+
+// Redirect to Customer Booking via Chat
+document.getElementById('left-button-chat')?.addEventListener('click', () => {
+  window.location.href = '/customer/AI-Booking.html';
 });
 
 
